dc7106f4fd0cc366b9a06b44c67362e83c711fc7
Adam Rogala <a.rogala@nowaera.pl>
2020-09-29 21:07:50 +0200

    add getters to GamesService

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_URL } from '@app/constants';
 import { Game, GameCategory } from '@app/models';
-import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -16,17 +16,25 @@ export class GamesService {
 
   private games: Game[];
 
+  private get gamesList(): Game[] {
+    return this.activeCategory$.value ? this.filteredGames : this.games;
+  }
+
+  private get filteredGames(): Game[] {
+    return this.games.filter((game) => game.categories.includes(this.activeCategory$.value));
+  }
+
   constructor(private readonly httpClient: HttpClient) {}
 
   loadGames(): void {
     if (this.games) {
-      this.visibleGames$.next(this.getFilteredGames());
+      this.visibleGames$.next(this.gamesList);
     }
 
     this.httpClient.get(`${API_URL}/games.php`)
       .pipe(
         tap((games) => this.games = games as Game[]),
-        tap(() => this.visibleGames$.next(this.getFilteredGames())),
+        tap(() => this.visibleGames$.next(this.gamesList)),
         tap(() => this.categories$.next(this.getCategories(this.games))),
       )
       .subscribe();
@@ -49,9 +57,5 @@ export class GamesService {
     });
 
     return categories;
-  }
-
-  private getFilteredGames(): Game[] {
-    return this.activeCategory$.value ? this.games.filter((game) => game.categories.includes(this.activeCategory$.value)) : this.games;
   }
 }
